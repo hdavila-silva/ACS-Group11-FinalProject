00b1c3b6a303d000b8c516517b5d5340b58690b9
Harmandeepghumman <118571983+Harmandeepghumman@users.noreply.github.com>
2022-12-23 20:32:03 -0500

    Delete infrastructure/networking directory

--- infrastructure/networking/config.tf
+++
@@ -1,8 +0,0 @@
-# S3 bucket for terraform state in dev networking environment
-terraform {
-  backend "s3" {
-    bucket = "group11-staging-private-bucket"
-    key    = "dev/network/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
